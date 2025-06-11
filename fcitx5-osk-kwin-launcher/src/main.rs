bce8e2fd751f2cedf2ee835e7995c4a930308454
fortime <palfortime@gmail.com>
2025-06-11 17:20:18 +0800

    feat(fcitx5-osk-kwin-launcher): support running in sddm
    Signed-off-by: fortime <palfortime@gmail.com>

--- fcitx5-osk-kwin-launcher/src/main.rs
+++ fcitx5-osk-kwin-launcher/src/main.rs
@@ -43,6 +43,10 @@ struct Args {
     /// Start as worker.
     #[arg(long, default_missing_value = "true")]
     worker: bool,
+
+    /// Start for sddm.
+    #[arg(long, default_missing_value = "true")]
+    sddm: bool,
 }
 
 async fn owner(
@@ -195,108 +199,87 @@ async fn watch_fcitx5(
 async fn watch_kwin_virtual_keyboard(
     fcitx5_osk_services: &Fcitx5OskServices,
     kwin_services: &KwinServices,
-    fdo_services: &FdoServices,
     in_lockscreen: bool,
 ) -> Result<()> {
-    let exit = Arc::new(AtomicBool::new(false));
-    tokio::spawn({
-        let fcitx5_osk_services = fcitx5_osk_services.clone();
-        let kwin_services = kwin_services.clone();
-        let exit = exit.clone();
-        let f = async move |fcitx5_osk_services: Fcitx5OskServices,
-                            kwin_services: KwinServices,
-                            exit: Arc<AtomicBool>,
-                            in_lockscreen: bool|
-                    -> Result<()> {
-            let expected_mode = if in_lockscreen {
-                WindowManagerMode::KwinLockScreen
-            } else {
-                WindowManagerMode::Normal
-            };
-            loop {
-                let mode = fcitx5_osk_services.controller().mode().await;
-                if (in_lockscreen && mode == Ok(expected_mode))
-                    || (!in_lockscreen && mode == Ok(expected_mode))
-                {
-                    break;
-                } else if exit.load(Ordering::Relaxed) {
-                    return Ok(());
-                } else {
-                    // make sure mode is set
-                    fcitx5_osk_services
-                        .controller()
-                        .change_mode(expected_mode)
-                        .await?;
-                }
-                tokio::time::sleep(Duration::from_millis(200)).await;
-            }
-            // in lockscreen, if the keyboard is set hidden, then trigger a hide-and-show of the keyboard to reset the input panel surface.
-            if in_lockscreen {
-                const TIMEOUT: Duration = Duration::from_secs(4);
-                const MAX_AMOUNT: usize = 5;
-                let mut visible_changes = VecDeque::with_capacity(MAX_AMOUNT);
-                let mut last_visible = true;
-                let mut stream = kwin_services
-                    .virtual_keyboard()
-                    .receive_visible_changed()
-                    .await?;
-                while stream.next().await.is_some() {
-                    let visible = kwin_services.virtual_keyboard().visible().await?;
-                    if exit.load(Ordering::Relaxed) {
-                        break;
-                    }
-                    tracing::debug!("kwin virtual keyboard visible: {visible}");
-                    let now = Instant::now();
-                    visible_changes.push_back(now);
-                    while visible_changes.len() >= MAX_AMOUNT {
-                        // In some cases, keyboard will be lost for ever in lockscreen. in these
-                        // cases, we should replace the input panel surface to let the keyboard
-                        // show again. If the user clicks the virtual keyboard button more than n
-                        // times during m seconds, we will replace the surface.
-                        if let Some(oldest) = visible_changes.pop_front() {
-                            if now.saturating_duration_since(oldest) < TIMEOUT {
-                                // show and hide will cause visible changed signal, so we remove
-                                // previous one in case it enters a infinite loop.
-                                visible_changes.clear();
-                                fcitx5_osk_services.controller().hide().await?;
-                                fcitx5_osk_services.controller().show().await?;
-                                continue;
-                            }
-                        }
-                    }
-                    if visible != last_visible {
-                        // only run if visible is different from the last one.
-                        fcitx5_osk_services
-                            .controller()
-                            .change_visible(visible)
-                            .await?;
-                    }
-                    last_visible = visible;
-                }
-            } else {
-                let mut stream = kwin_services
-                    .virtual_keyboard()
-                    .receive_active_changed()
-                    .await?;
-                while stream.next().await.is_some() {
-                    let active = kwin_services.virtual_keyboard().active().await?;
-                    let tablet_mode = kwin_services.tablet_mode().tablet_mode().await?;
-                    if exit.load(Ordering::Relaxed) {
-                        break;
-                    }
-                    // check tablet mode, show only if it is in tablet mode.
-                    tracing::debug!(
-                        "kwin virtual keyboard active: {active}, tablet mode: {tablet_mode}"
-                    );
-                    if active && tablet_mode {
+    let expected_mode = if in_lockscreen {
+        WindowManagerMode::KwinLockScreen
+    } else {
+        WindowManagerMode::Normal
+    };
+    loop {
+        let mode = fcitx5_osk_services.controller().mode().await;
+        if (in_lockscreen && mode == Ok(expected_mode))
+            || (!in_lockscreen && mode == Ok(expected_mode))
+        {
+            break;
+        } else {
+            // make sure mode is set
+            fcitx5_osk_services
+                .controller()
+                .change_mode(expected_mode)
+                .await?;
+        }
+        tokio::time::sleep(Duration::from_millis(200)).await;
+    }
+    // in lockscreen, if the keyboard is set hidden, then trigger a hide-and-show of the keyboard to reset the input panel surface.
+    if in_lockscreen {
+        const TIMEOUT: Duration = Duration::from_secs(4);
+        const MAX_AMOUNT: usize = 5;
+        let mut visible_changes = VecDeque::with_capacity(MAX_AMOUNT);
+        let mut last_visible = true;
+        let mut stream = kwin_services
+            .virtual_keyboard()
+            .receive_visible_changed()
+            .await?;
+        while stream.next().await.is_some() {
+            let visible = kwin_services.virtual_keyboard().visible().await?;
+            tracing::debug!("kwin virtual keyboard visible: {visible}");
+            let now = Instant::now();
+            visible_changes.push_back(now);
+            while visible_changes.len() >= MAX_AMOUNT {
+                // In some cases, keyboard will be lost for ever in lockscreen. in these
+                // cases, we should replace the input panel surface to let the keyboard
+                // show again. If the user clicks the virtual keyboard button more than n
+                // times during m seconds, we will replace the surface.
+                if let Some(oldest) = visible_changes.pop_front() {
+                    if now.saturating_duration_since(oldest) < TIMEOUT {
+                        // show and hide will cause visible changed signal, so we remove
+                        // previous one in case it enters a infinite loop.
+                        visible_changes.clear();
+                        fcitx5_osk_services.controller().hide().await?;
                         fcitx5_osk_services.controller().show().await?;
+                        continue;
                     }
                 }
             }
-            Ok(())
-        };
-        f(fcitx5_osk_services, kwin_services, exit, in_lockscreen)
-    });
+            if visible != last_visible {
+                // only run if visible is different from the last one.
+                fcitx5_osk_services
+                    .controller()
+                    .change_visible(visible)
+                    .await?;
+            }
+            last_visible = visible;
+        }
+    } else {
+        let mut stream = kwin_services
+            .virtual_keyboard()
+            .receive_active_changed()
+            .await?;
+        while stream.next().await.is_some() {
+            let active = kwin_services.virtual_keyboard().active().await?;
+            let tablet_mode = kwin_services.tablet_mode().tablet_mode().await?;
+            // check tablet mode, show only if it is in tablet mode.
+            tracing::debug!("kwin virtual keyboard active: {active}, tablet mode: {tablet_mode}");
+            if active && tablet_mode {
+                fcitx5_osk_services.controller().show().await?;
+            }
+        }
+    }
+    Ok(())
+}
+
+async fn watch_lockscreen_state(fdo_services: &FdoServices, in_lockscreen: bool) -> Result<()> {
     let mut stream = fdo_services.screen_saver().receive_active_changed().await?;
     while let Some(changed) = stream.next().await {
         let active = changed.args()?.active;
@@ -306,7 +289,6 @@ async fn watch_kwin_virtual_keyboard(
             break;
         }
     }
-    exit.store(true, Ordering::Relaxed);
     Ok(())
 }
 
@@ -374,22 +356,42 @@ async fn run(args: &Args) -> Result<()> {
         }
     });
 
+    let watch_fcitx5_fut = watch_fcitx5(
+        &connection,
+        fcitx5_socket,
+        wayland_display.clone(),
+        args.fcitx5_reopen,
+        shutdown_flag.clone(),
+    );
+
     // only the latest match rule will work in zbus::receive_signal. so I create two connections.
     tokio::select! {
-        res = watch_fcitx5(
-            &connection,
-            fcitx5_socket,
-            wayland_display.clone(),
-            args.fcitx5_reopen,
-            shutdown_flag.clone(),
-        ) => {
+        res = {
+            let mut shutdown_flag = shutdown_flag.clone();
+            async move {
+                if lockscreen_active {
+                    // there is no need to watch_fcitx5 in lockscreen mode, wait shutting down.
+                    shutdown_flag.wait_for_shutdown().await;
+                    Ok(())
+                } else {
+                    watch_fcitx5_fut.await
+                }
+            }
+        } => {
             if let Err(e) = res {
                 tracing::error!("watch_fcitx5 exits abnormally: {e:?}");
             } else {
                 tracing::info!("watch_fcitx5 exits");
             }
         }
-        res = watch_kwin_virtual_keyboard(&services, &kwin_services, &fdo_services, lockscreen_active) => {
+        res = watch_lockscreen_state(&fdo_services, lockscreen_active) => {
+            if let Err(e) = res {
+                tracing::error!("watch_lockscreen_state exits abnormally: {e:?}");
+            } else {
+                tracing::info!("the state of lockscreen is changed");
+            }
+        }
+        res = watch_kwin_virtual_keyboard(&services, &kwin_services, lockscreen_active) => {
             if let Err(e) = res {
                 tracing::error!("watch_kwin_virtual_keyboard exits abnormally: {e:?}");
             } else {
@@ -460,11 +462,103 @@ async fn daemon() -> Result<()> {
     Ok(())
 }
 
+async fn run_in_sddm(args: &Args) -> Result<()> {
+    let _log_guard = fcitx5_osk_common::log::init_log(&[], args.log_timestamp)?;
+
+    let (mut shutdown_flag, signal_handle) = fcitx5_osk_common::signal::shutdown_flag();
+    tokio::spawn(signal_handle);
+
+    let socket = match env::var("WAYLAND_SOCKET")
+        .unwrap_or_default()
+        .parse::<i32>()
+    {
+        Ok(socket) => {
+            let socket = unsafe { OwnedFd::from_raw_fd(socket) };
+            Some(socket)
+        }
+        Err(_) => None,
+    };
+    let wayland_display = env::var("WAYLAND_DISPLAY").unwrap_or_default();
+
+    // tracing can format argument whose name is display.
+    tracing::debug!(
+        "wayland socket: {:?}, wayland display: {}",
+        socket,
+        wayland_display
+    );
+
+    let connection = Connection::session().await?;
+
+    let services = Fcitx5OskServices::new().await?;
+    let kwin_services = KwinServices::new_with(&connection).await?;
+
+    let fcitx5_osk_exited = Arc::new(AtomicBool::new(false));
+    let fcitx5_osk_handler = tokio::spawn({
+        let connection = connection.clone();
+        let wayland_display = wayland_display.clone();
+        let shutdown_flag = shutdown_flag.clone();
+        let fcitx5_osk_exited = fcitx5_osk_exited.clone();
+        async move {
+            if let Err(e) =
+                watch_fcitx5_osk(&connection, socket, wayland_display, shutdown_flag.clone()).await
+            {
+                tracing::error!("watch_fcitx5_osk exits abnormally: {e:?}");
+            } else {
+                tracing::info!("watch_fcitx5_osk exits");
+            }
+            // set fcitx5_osk_exited to true before shutting down.
+            fcitx5_osk_exited.store(true, Ordering::Relaxed);
+            // tell the main loop to exit
+            shutdown_flag.shutdown();
+        }
+    });
+
+    // only the latest match rule will work in zbus::receive_signal. so I create two connections.
+    tokio::select! {
+        res = watch_kwin_virtual_keyboard(&services, &kwin_services, true) => {
+            if let Err(e) = res {
+                tracing::error!("watch_kwin_virtual_keyboard exits abnormally: {e:?}");
+            } else {
+                tracing::info!("watch_kwin_virtual_keyboard exits");
+            }
+        }
+        _ = shutdown_flag.wait_for_shutdown() => {
+        }
+    }
+
+    let mut shutdown_res = None;
+    if !fcitx5_osk_exited.load(Ordering::Relaxed) {
+        // shutdown fcitx5-osk
+        shutdown_res = Some(services.controller().shutdown().await);
+        // wait fcitx5-osk to shutdown
+        let _ = fcitx5_osk_handler.await;
+    }
+
+    // disable and enable virtual keyboard to restart launcher
+    let disable_res = kwin_services.virtual_keyboard().set_enabled(false).await;
+    let enable_res = kwin_services.virtual_keyboard().set_enabled(true).await;
+    tracing::info!(
+        "shutdown fcitx5-osk result: {:?}, disable virtual keyboard result: {:?}, enable virtual keyboard result: {:?}",
+        shutdown_res,
+        disable_res,
+        enable_res
+    );
+
+    // wait a moment for letting fcitx5-osk to shutdown gracefully.
+    tokio::time::sleep(Duration::from_secs(1)).await;
+
+    Ok(())
+}
+
 #[tokio::main]
 pub async fn main() {
     let args = Args::parse();
     let res = if args.worker {
-        run(&args).await
+        if args.sddm {
+            run_in_sddm(&args).await
+        } else {
+            run(&args).await
+        }
     } else {
         daemon().await
     };
