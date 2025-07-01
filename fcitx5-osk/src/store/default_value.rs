4cfd82b71fc289b6e025ee8a2fe1079d49673799
fortime <palfortime@gmail.com>
2025-07-01 17:49:11 +0800

    style: cargo fmt
    Signed-off-by: fortime <palfortime@gmail.com>

--- fcitx5-osk/src/store/default_value.rs
+++ fcitx5-osk/src/store/default_value.rs
@@ -1,5 +1,7 @@
-pub const DEFAULT_LANDSCAPE_KEY_AREA_LAYOUT_TOML: &str = include_str!("../../../assets/layouts/default-landscape.toml");
+pub const DEFAULT_LANDSCAPE_KEY_AREA_LAYOUT_TOML: &str =
+    include_str!("../../../assets/layouts/default-landscape.toml");
 
-pub const DEFAULT_PORTRAIT_KEY_AREA_LAYOUT_TOML: &str = include_str!("../../../assets/layouts/default-portrait.toml");
+pub const DEFAULT_PORTRAIT_KEY_AREA_LAYOUT_TOML: &str =
+    include_str!("../../../assets/layouts/default-portrait.toml");
 
 pub const DEFAULT_KEY_SET_TOML: &str = include_str!("../../../assets/key_sets/default.toml");
